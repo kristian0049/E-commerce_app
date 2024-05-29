f4abab16c774860485989172a23ae9a4374286a6
kriskobeast@gmail.com <kriskobeast@gmail.com>
2024-05-29 20:15:19 +0100

    Added node-postgres

--- backend/index.js
+++ backend/index.js
@@ -2,16 +2,30 @@ const express = require ('express');
 const app = express();
 const port = 3000;
 
-const pg = require('pg');
+const db = require('./db/index.js');
 
 
 
-app.get('/', (req, res) => {
-    res.send('Hello world! + '+ _res );
-   
+app.get('/', async (req, res) => {
+    const data = await db.query('SELECT NOW()',"");
+    res.send( ""+data.rows[0].now );
 });
 
 app.listen(port, () => {
     console.log('Example app listening on port' + port);
 });
 
+
+//API endpoint to create a user
+
+//API endpoint to check user log in details, fetch and log in
+
+//API endpoint to set user personnel details, user address , user_payment
+
+//API endpoiint to fetch user payment if saved, user address
+
+//API endpoint to create a shopping session
+//api endpoint to modify shoppint session
+
+//API endpoint to store cart items
+//api endpoint to update cart items if added or removed 
