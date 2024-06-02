b7a461cf7ecb74e61bd533a323dfb0f5bd7854bb
kriskobeast@gmail.com <kriskobeast@gmail.com>
2024-06-02 20:52:40 +0100

    Added categories of games into the database

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,6 @@ const port = 3000;
 
 const db = require('./db/index.js');
 
-
-
 app.get('/', async (req, res) => {
     const data = await db.query('SELECT NOW()',"");
     res.send( ""+data.rows[0].now );
@@ -14,6 +12,3 @@ app.get('/', async (req, res) => {
 app.listen(port, () => {
     console.log('Example app listening on port' + port);
 });
-
-
-
--- backend/loadData.js
+++ backend/loadData.js
@@ -1,17 +1,15 @@
 //example of api https://api.mobygames.com/v1/genres?api_key=MOBYGAMES_API
 require('dotenv').config();
 const api = process.env.MOBYGAMES_API;
+const superagent = require('superagent');
+const db = require('./db/index.js');
+const url = `https://api.mobygames.com/v1/genres?api_key=${api}`;
 
-const express = require ('express');
-const app = express();
-const port = 3000;
+//need to create a query to add games to db
 
+superagent.get(url)
+.set('accept','json')
+.end(async (err,res)=>{
+ 
 
-app.get(`https://api.mobygames.com/v1/genres?api_key=${api}`, async (req, res) => {
-    console.log(res);
 });
-
-app.listen(port, () => {
-    console.log('Example app listening on port' + port);
-});
-
